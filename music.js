505b3cc34514600e0ed608bd23b515d75e7eff83
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-08-29 09:47:00 -0700

    Update music.js

--- music.js
+++ music.js
@@ -1,30 +1,44 @@
 function preload() {
   soundFormats('mp3', 'ogg','wav');
   AlphysTakesAction = loadSound('Alphys takes action.mp3');
-  Asgore = loadSound('Asgore.mp3');
+  AsgoreIntro = loadSound('AsgoreIntro.mp3');
+  HopesAndDreams = loadSound('Hopes and Dreams.mp3');
 
 }
 
 function setup() {
   AlphysTakesAction.setVolume(2.5);
-    Asgore.setVolume(2.5);
-
+  AsgoreIntro.setVolume(2.5);
+  HopesAndDreams.setVolume(2.5);
   button = createButton('AlphysTakesAction');
-  button.mousePressed(playSong);
-  button2 = createButton('Asgore');
+  button.mousePressed(playSong1);
+  button2 = createButton('Asgore Intro');
   button2.mousePressed(playSong2);
+  button3 = createButton('Hopes And Dreams');
+  button3.mousePressed(playSong3);
 }
 
-function playSong() {
+function playSong1() {
   AlphysTakesAction.stop(); 
-  Asgore.stop(); 
+  AsgoreIntro.stop(); 
+  HopesAndDreams.stop(); 
   AlphysTakesAction.play(); 
   
 }
 
 function playSong2() {
 AlphysTakesAction.stop(); 
-  Asgore.stop(); 
-  Asgore.play(); 
+  AsgoreIntro.stop(); 
+    HopesAndDreams.stop(); 
+
+  AsgoreIntro.play(); 
+  
+}
+
+function playSong3() {
+  AlphysTakesAction.stop(); 
+  AsgoreIntro.stop(); 
+  HopesAndDreams.stop(); 
+  HopesAndDreams.play(); 
   
 }
