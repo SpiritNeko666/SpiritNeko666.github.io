7b53ccdca5115d87c160a864888083974c16f9db
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-09-09 11:23:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function setup() {
   createCanvas(400, 400);
-  resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
-  background(0);  
+  background(0); 
+    resizeCanvas(windowWidth, windowHeight);
+
 }
