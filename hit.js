27559e290e5c21588e93382f94778e9433500311
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-09-16 10:11:41 -0700

    Update hit.js

--- hit.js
+++ hit.js
@@ -1,1 +1,20 @@
 
+ function Hit(x, y) {
+  this.x = x;
+  this.y = y;
+  this.r = 30;
+  this.xdir = 4;
+  this.shiftDown = function() { 
+   this.xdir *= -1;
+  this.y += this.r;
+   }
+  
+  this.move = function() {
+  this.x = this.x + this.xdir;
+  }
+  
+  this.show = function() {
+    fill(255, 0, 200);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+}
