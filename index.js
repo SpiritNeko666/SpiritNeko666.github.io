10a55d9e729abb55434f5c01421a797ee0733f48
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-09-27 11:13:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ function setup() {
 
 function draw() {
   background(102);
-  //resizeCanvas(windowWidth, windowHieght);
 
   
+  
+  push();
   fill('lime')
   stroke('black');
-  push();
   translate(width * 0.5, height * 0.5);
   rotate(frameCount / 50.0);
-  star(0, 0, 55, 10, 10);
+  star(0, 0, 55, 15, 10);
   pop();
   push();
-  
+  fill('lime')
+  stroke('black');
   translate(width * 0.5, height * 0.5);
   rotate(frameCount / -50.0);
   star(0, 0, 55, 15, 10);
+  pop()
+  
+  push();
+  fill('white')
+  stroke('crimson');
+  translate(width * 0.2, height * 0.5);
+  rotate(frameCount / 50.0);
+  star1(0, 0, 55, 15, 15);
+  pop();
+   push();
+  fill('white')
+  stroke('crimson');
+  translate(width * 0.2, height * 0.5);
+  rotate(frameCount / -50.0);
+  star1(0, 0, 55, 15, 15);
   pop();
 }
 //Turn in: Your own stars with color.
@@ -33,6 +49,20 @@ function star(x, y, radius1, radius2, npoints) {
     sx = x + cos(a + halfAngle) * radius1;
     sy = y + sin(a + halfAngle) * radius1;
     vertex(x, sy);
+  }
+  endShape(CLOSE);
+}
+function star1(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x * cos(a - halfAngle) * radius1;
+    sy = y + sin(a - halfAngle) * radius1;
+    vertex(sy, sx);
   }
   endShape(CLOSE);
 }
