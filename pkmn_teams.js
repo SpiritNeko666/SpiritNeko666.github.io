bcb8fc29d118eb9d5a6411fa23b67de0c8c7be1a
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2020-07-15 13:38:09 -0700

    Update pkmn_teams.js

--- pkmn_teams.js
+++ pkmn_teams.js
@@ -1,8 +1,7 @@
 function setup() {
-createA('PkMn/All_Teams.txt', 'All Teams');
-createA('PkMn/Team_1.txt', 'Team_1');
+createA('PkMn/All_Files.txt', 'All Files');
 createP('');
-
+createA('PkMn/Team_1.txt', 'Team_1');
 createA('PkMn/Team_2.txt', 'Team_2');
 createA('PkMn/Team_3.txt', 'Team_3');
 createA('PkMn/Team_4.txt', 'Team_4');
