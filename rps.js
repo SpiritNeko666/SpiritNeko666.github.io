76d0b6c8fc18865d799568cb51765ba5270c427a
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2026-08-26 13:17:10 -0700

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
+function setup() {
+createA('PkMn/All_Files.txt', 'All Files');
+createP('');
 
+createA('PkMn/Tournament_Teams.txt', 'Tournament Teams');
+
+createP('');
+createA('PkMn/Team_1.txt', 'Team_1');
+createA('PkMn/Team_2.txt', 'Team_2');
+createA('PkMn/Team_3.txt', 'Team_3');
+createA('PkMn/Team_4.txt', 'Team_4');
+}
